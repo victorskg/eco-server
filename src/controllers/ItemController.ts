ef65397633ede980fd80bd248e7619a9625769c8
victorskg <victorskg@outlook.com>
2020-06-09 11:38:19 -0300

    Alteração para recebimento de request mobile

--- src/controllers/ItemController.ts
+++ src/controllers/ItemController.ts
@@ -3,12 +3,15 @@ import { Request, Response } from "express";
 
 class ItemController {
   async index(request: Request, response: Response) {
+    const baseUrl = request.query.app
+      ? "http://192.168.0.121:3333/uploads"
+      : "http://localhost:3333/uploads/";
     const items = await knex("item").select("*");
     const serializedItems = items.map((item) => {
       return {
         id: item.id,
         title: item.title,
-        imageUrl: `http://localhost:3333/uploads/${item.image}`,
+        imageUrl: `${baseUrl}/${item.image}`,
       };
     });
     return response.json(serializedItems);
