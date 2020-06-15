c4380fe2cb381320091d4cdc953f2b60209d34f0
victorskg <victorskg@outlook.com>
2020-06-15 17:37:29 -0300

    Validação durante cadastro de ponto.

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import ItemController from "./controllers/ItemController";
 import PointController from "./controllers/PointController";
 import multer from "multer";
 import multerConfig from "./config/multer";
+import { celebrate, Joi } from "celebrate";
 
 const routes = express.Router();
 const upload = multer(multerConfig);
@@ -13,7 +14,23 @@ const pointController = new PointController();
 routes.get("/items", itemController.index);
 
 routes.get("/points", pointController.index);
-routes.post("/points", upload.single("image"), pointController.create);
 routes.get("/points/:id", pointController.show);
+routes.post(
+  "/points",
+  upload.single("image"),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(),
+    }),
+  }),
+  pointController.create
+);
 
 export default routes;
